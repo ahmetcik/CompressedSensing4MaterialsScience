ca8511367e2b121366ac03b8c4ab089f1daadcd3
Emre Ahmetcik <ahmetcik@fhi-berlin.mpg.de>
2019-03-27 12:19:05 +0100

    requirements

---
+++ requirements/install_requirements.sh
@@ -0,0 +1,22 @@
+
+#pandas
+pip install pandas
+
+#matplotlib
+pip install matplotlib
+
+# sklearn
+pip install sklearn
+
+# bokeh
+pip install bokeh
+
+# ase
+pip install ase
+
+# nglview
+pip install nglview
+
+# numpy
+pip install numpy
+#or pip install numpy==1.13.3
